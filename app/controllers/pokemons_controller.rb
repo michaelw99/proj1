e26ece4c7bbbe6a5ad9cec76dde67feab02a49b3
Michael Wan <michaelw99@berkeley.edu>
2015-04-03 01:38:05 -0700

    finished proj1

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,17 +3,17 @@ class PokemonsController < ApplicationController
 		@pokemon = Pokemon.find(params[:id])
 		@pokemon.trainer = current_trainer
 		@pokemon.save
-		redirect_to 'home#index'
+		redirect_to :back
     end
 
     def damage
         @pokemon = Pokemon.find(params[:id])
         @pokemon.health = @pokemon.health - 10
+        if @pokemon.save
+            redirect_to :back
+        end
         if @pokemon.health <= 0
             @pokemon.destroy
-        end
-        if @pokemon.save
-            redirect_to 'trainers#id'
         end
     end
 
@@ -23,14 +23,15 @@ class PokemonsController < ApplicationController
 
     def create
         @pokemon = Pokemon.create(pokemon_params)
+        @id = current_trainer.id
         @pokemon.health = 100
         @pokemon.level = 1
         @pokemon.trainer = current_trainer
         if @pokemon.save 
-            redirect_to 'trainers#id'
+            redirect_to "/trainers/" + @id.to_s
         else 
             flash[:error] = @pokemon.errors.full_messages.to_sentence
-            redirect_to 'home#index'
+            redirect_to :back
         end  
     end
 
